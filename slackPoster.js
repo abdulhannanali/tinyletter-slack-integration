296372dcd1d3e5b4e3c2a594172e3b5e56c70dfc
Hannan Ali <abdulhannanali@outlook.com>
2017-05-27 18:40:05 +0500

    make it more abstract

--- slackPoster.js
+++ slackPoster.js
@@ -3,7 +3,11 @@
  * Post new articles to slack so people can learn about the content.
  */
 
-const { SLACK_WEBHOOK_URL, AUTHOR_TWITTER_HANDLE } = process.env;
+const { 
+    SLACK_WEBHOOK_URL, 
+    AUTHOR_TWITTER_HANDLE,
+    NEWSLETTER_NAME
+ } = process.env;
 const axios = require('axios');
 
 function postToSlack(article) {
@@ -16,9 +20,10 @@ function formatSlackMessage (article) {
 
     const text = `${twitter_link} posted a :fire: tiny letter with wisdom for champs :clap: :raised_hands: :partyparrot:`;
 
+    const fallbackText = `${AUTHOR_TWITTER_HANDLE} posted a tinyletter on ${article.date}, ${author.title} ${author.link}`
     const attachments = [
         {
-            fallback: article.title + ' ' + article.link,
+            fallback: fallbackText,
             title: article.title,
             title_link: article.link,
             text: article.snippet,
@@ -30,7 +35,9 @@ function formatSlackMessage (article) {
                     short: false
                 }
             ],
-            footer: 'cassidoo\'s tinyletter archive'
+            author_name: '@' + AUTHOR_TWITTER_HANDLE,
+            author_link: 'https://twitter.com/@' + AUTHOR_TWITTER_HANDLE,
+            footer: NEWSLETTER_NAME + '\'s tinyletter archive'
         }
     ];
 
